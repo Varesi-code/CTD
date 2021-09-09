c21f4302dd300687862e8178cfe67a74e4e83078
Natalia Varesi <varesinatalia@gmail.com>
2021-09-09 10:51:10 -0300

    main

--- 04-Progimp/clase13/boilerPlateObjetos.js
+++ 04-Progimp/clase13/boilerPlateObjetos.js
@@ -1,37 +1,4 @@
-//lista de clientes.
-let arrayCuentas = [
-    {
-      nroCuenta: 5486273622,
-      tipoDeCuenta: "Cuenta Corriente",
-      saldoEnPesos: 27771,
-      titularCuenta: "Abigael Natte",
-    },
-    {
-      nroCuenta: 1183971869,
-      tipoDeCuenta: "Caja de Ahorro",
-      saldoEnPesos: 8675,
-      titularCuenta: "Ramon Connell",
-    },
-    {
-      nroCuenta: 9582019689,
-      tipoDeCuenta: "Caja de Ahorro",
-      saldoEnPesos: 27363,
-      titularCuenta: "Jarret Lafuente",
-    },
-    {
-      nroCuenta: 1761924656,
-      tipoDeCuenta: "Cuenta Corriente",
-      saldoEnPesos: 32415,
-      titularCuenta: "Ansel Ardley",
-    },
-    {
-      nroCuenta: 7401971607,
-      tipoDeCuenta: "Cuenta Unica",
-      saldoEnPesos: 18789,
-      titularCuenta: "Jacki Shurmer",
-    },
-  ];
-  // ANCHOR podes continuar tu codigo a partir de aca!
+
   //     3) y 4)
   const banco = {
     clientes: arrayCuentas,
@@ -68,3 +35,92 @@ let arrayCuentas = [
   console.log(banco.consultarCliente("Jacki Shurmer"));
   console.log(banco.deposito("Jacki Shurmer", 10000));
   console.log(banco.extraccion("Jacki Shurmer", 1000));
+
+
+
+//   Descargar y analizar el siguiente archivo, el cual contiene la lista de clientes
+let arrayCuentas = [
+  {
+    nroCuenta: 5486273622,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 27771,
+    titularCuenta: "Abigael Natte",
+  },
+  {
+    nroCuenta: 1183971869,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 8675,
+    titularCuenta: "Ramon Connell",
+  },
+  {
+    nroCuenta: 9582019689,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 27363,
+    titularCuenta: "Jarret Lafuente",
+  },
+  {
+    nroCuenta: 1761924656,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 32415,
+    titularCuenta: "Ansel Ardley",
+  },
+  {
+    nroCuenta: 7401971607,
+    tipoDeCuenta: "Cuenta Unica",
+    saldoEnPesos: 18789,
+    titularCuenta: "Jacki Shurmer",
+  },
+];
+
+// 2. Nos solicitan también crear un objeto literal llamado “banco”, el cual tendrá una
+// propiedad llamada “clientes”, que estará compuesta de la lista de objetos
+// obtenidos en el punto anterior.
+
+// 3. Al objeto “banco”, crearle un método llamado consultarCliente, el cual recibirá
+// un nombre (titular) por parámetro, deberá buscarlo en la lista de cuentas y
+// retornar el objeto cliente que corresponda con ese nombre ingresado.
+// ayuda: let clienteEncontrado = banco.consultarCliente(“Ramon Connell”);
+
+// 4. Crear otro método llamado depósito, que recibirá dos parámetros, el titular de
+// cuenta y una cantidad de dinero a depositar. El método debe obtener a la
+// cuenta correspondiente y luego sumar la cantidad de dinero a depositar a saldo
+// en pesos. Luego, deberá dar un aviso por la consola con el mensaje “Depósito
+// realizado, su nuevo saldo es: xxx”.
+
+// 5. Crear un último método llamado extracción, que recibirá también dos
+// parámetros: el titular de cuenta y el monto a extraer. El método debe obtener
+// la cuenta correspondiente y restar el monto al saldo actual. En caso de que el
+// resultado sea menor a 0, deberá imprimir un mensaje por la consola de
+// “Fondos insuficientes”, de lo contrario deberá imprimir “Extracción realizada
+// correctamente, su nuevo saldo es: xxx”.
+// Si llegaste hasta acá, felicidades, el equipo de desarrollo y el tech leader están
+// impresionados con tu trabajo.
+
+
+// Bonus extra
+// Para que no te quedes con las ganas y puedas seguir practicando si así lo deseas, te
+// proponemos algunos ejercicios más. Tené en cuenta que a partir de acá los ejercicios
+// pueden escalar en dificultad. Como siempre decimos, paciencia, ignorá la complejidad
+// y tratá de resolverlo con las herramientas que tengas a tu disposición. También podés
+// buscar información extra en Google o documentaciones que conozcas.
+
+// Propiedad única
+// Crear una función llamada propiedadUnica, que reciba un arreglo de objetos como
+// parámetro y un string. Esta deberá retornar un nuevo arreglo de objetos, teniendo
+// como parámetro la propiedad que fue pasada como string.
+// Ejemplo:
+// let array = [ { nombre: “Lean”, edad: 27 }, { nombre: “Eze”, edad: 49} ]
+// propiedadUnica(array, “edad”) debe retornar [ { edad: 27 }, { edad: 49 } ]
+// propiedadUnica(array, “nombre”) debe retornar [ { nombre: “Lean”}, { nombre: “Eze” } ]
+
+// Calculador de notas
+// Crear el objeto “alumno”, el cual va a consistir en las siguientes propiedades básicas:
+// ● Nombre
+// ● Número de legajo
+// ● Lista de notas
+
+
+// Nos gustaría calcular el promedio del alumno y si el mismo está aprobado, basado en
+// una nota de aprobación que le va a ser dada. Para este ejercicio, vamos a dejar que
+// pienses todos los métodos y propiedades que puedan hacer falta para que el
+// programa funcione correctamente de la manera solicitada.
